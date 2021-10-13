78951670b2bd181542bd28a7c7794d2508e316b9
easyiom <100007563.joan23@fje.edu>
2021-10-13 17:15:42 +0200

    aplicacion acabada

--- js/js.js
+++ js/js.js
@@ -3,10 +3,14 @@ function triangulo() {
     console.log(n);
     n++
     var result = ""
-    for (let i = 1; i < n; i++) {
-        console.log(result);
-        let str = "*";
-        result += str.repeat(i) + '</br>'
+    if (isNaN(n) == true || n == 0 || n < 0) {
+        swal('Error', 'Introduce un valor valido *no puede ser 0, un valor más pequeño que 0, numero no entero...', 'error')
+    } else {
+        for (let i = 1; i < n; i++) {
+            console.log(result);
+            let str = "*";
+            result += str.repeat(i) + '</br>'
+        }
+        document.getElementById('triangulo').innerHTML = result
     }
-    document.getElementById('triangulo').innerHTML = result
 }
